758e04014fcd58c59ea68684b5b3a1ebe245143b
SachaBogaers <san_bogaers@hotmail.com>
2021-03-06 12:13:49 +0100

    first exercise

---
+++ exercise_1/script.js
@@ -0,0 +1,19 @@
+// zet de functie om naar een arrow functie
+const ikRockArrowFunctions = () => {
+	return "Joe, ik rock de arrow functions!"
+};
+
+const fivePlusSeven = () => 5 + 7;
+
+const myFunction = (a, b) => a + b;
+
+const addFive = a => a + 5;
+
+const createObject = () => ({ greeting: "hoi" });
+
+console.log(ikRockArrowFunctions());
+console.log("test");
+console.log(fivePlusSeven());
+console.log(myFunction(2, 8));
+console.log(addFive(5));
+console.log(createObject());
